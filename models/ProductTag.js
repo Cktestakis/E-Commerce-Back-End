3297ce43514aec955b106cf9a242ed1ac481de60
Cktestakis <codyktestakis@gmail.com>
2021-10-29 11:03:18 -0400

    ProductTag created

---
+++ models/ProductTag.js
@@ -0,0 +1,32 @@
+const { Mode, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "product",
+                key: "id",
+            },
+        },
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "tag",
+                key: "id",
+            },
+        },
+    },
+);
+
+module.exports = ProductTag;
